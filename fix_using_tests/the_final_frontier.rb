23c8f0cee2d78a4f71ca944d11286065b209543d
casual-karma-e108 <akravecas@yahoo.com>
2021-02-16 17:38:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -16,3 +16,20 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
+
+
+
+def generate_star_date
+  (rand(100000) + 400000) / 10.0
+end
+
+
+def state_log(generate_star_date)
+  "Captain's Log, star date #{generate_star_date}."
+end
+
+
+def engage
+  puts state_log(date)
+  date = generate_star_date
+end
